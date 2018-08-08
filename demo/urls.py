17963fc62ad878600b8d13cb82d88cf0827f8159
srikanthpragada <srikanthpragada@gmail.com>
2018-08-08 21:01:12 +0530

    08-AUG

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from . import views
+from . import views, hr_views
 
 urlpatterns = [
     path('index/', views.index),
+    path('discount/', views.discount),
+    path('adddept/', hr_views.adddept),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
-from . models import Info
+from .models import Info
 from datetime import datetime
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,'index.html',
+    return render(request, 'index.html',
                   {
-                    "info" : Info(),
-                    "now" : str(datetime.now())
+                      "info": Info(),
+                      "now": str(datetime.now())
                   }
-                )
+                  )
+
+
+def discount(request):
+    discount = 0
+    amount = ''
+    rate = ''
+    message = ""
+    if request.method == "POST":
+        try:
+            amount = float(request.POST["amount"])
+            rate = float(request.POST["rate"])
+            discount = amount * rate / 100
+        except:
+            message = "Invalid Input. Please enter valid numbers for amount and rate!"
+
+    return render(request, 'discount.html',
+                  {'discount': discount,
+                   'amount': amount,
+                   'rate': rate,
+                   'message' : message}
+                 )
